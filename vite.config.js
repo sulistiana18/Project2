742863d6ea0c595381f18045f244bdd896c98392
sulistiana18 <suliez.tiana16@gmail.com>
2025-12-30 15:07:54 +0700

    30 des - 1

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-    // allowedHosts: true,
-    
     hmr:
       process.env.CODESANDBOX_SSE || process.env.GITPOD_WORKSPACE_ID
         ? 443
